78b31a9a802be6e5619bff65e50ce3b4c2ae50d3
antoniostassi <stassiant@outlook.it>
2024-07-11 15:03:29 +0200

    HTML & CSS | Making Button reactive to JS

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Control Variables
+let checkAnswer1 = false;
+let checkAnswer2 = false;
+let userMessage = document.getElementById("user-message");
+
+
+// Button Var
+const chatInput = document.getElementById("send-message");
+// console.log(chatInput);
+
+chatInput.addEventListener("click", (e) => {
+
+    // console.log("Debug | Click");
+
+    if (checkAnswer1 == false) {
+        // console.log("Debug | Answer1");
+        
+    }
+
+
+})
